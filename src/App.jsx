1457e6c2666a31a0eec5a9600381ebf8a34b1afb
Yishin-Jheng <lf90691@gmail.com>
2026-05-29 12:31:12 +0800

    optim: 完成 SCSS to Tailwind CSS 的樣式重構

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,36 @@ import dividerDesktop from "./img/pattern-divider-desktop.svg";
 import dividerMobile from "./img/pattern-divider-mobile.svg";
 import buttonImg from "./img/icon-dice.svg";
 
+const loadingAnimationClass =
+  "w-5 h-5 rounded-full bg-green animate-pulse-bubble";
+
 function App() {
   const { fetchAdvice, isLoading, isError, adviceContent } = useAdvice();
 
   return (
-    <main className="container">
-      <div className="adviceId">
-        <span className="adviceId__text">Advice</span>#
-        <span className="adviceId__id">{adviceContent.id}</span>
+    <main className="relative max-w-200 min-h-100 p-10 mx-auto my-0 flex flex-col justify-between text-center bg-grey-dark-2 rounded-2xl max-md:w-[calc(100%-60px)] max-md:px-5 max-sm:w-[calc(100%-30px)] max-sm:px-3.75">
+      <div className="text-green uppercase tracking-[4px] text-[18px] max-md:text-[15px]">
+        <span>Advice</span>
+        <span>{`#${adviceContent.id}`}</span>
       </div>
       {isLoading && (
-        <div className="loading">
-          <div className="loading__bubble loading__bubble-1">&nbsp;</div>
-          <div className="loading__bubble loading__bubble-2">&nbsp;</div>
-          <div className="loading__bubble loading__bubble-3">&nbsp;</div>
+        <div className="flex gap-5 justify-center">
+          <div className={`${loadingAnimationClass} [animation-delay:0s]`}>
+            &nbsp;
+          </div>
+          <div className={`${loadingAnimationClass} [animation-delay:0.2s]`}>
+            &nbsp;
+          </div>
+          <div className={`${loadingAnimationClass} [animation-delay:0.4s]`}>
+            &nbsp;
+          </div>
         </div>
       )}
       {!isLoading && (
-        <div className="adviceContent">
-          {!isError && (
-            <span className="adviceContent__text">{adviceContent.advice}</span>
-          )}
+        <div className="text-cyan font-extrabold text-[32px] max-md:text-[28px] max-sm:text-[24px]">
+          {!isError && <span>{adviceContent.advice}</span>}
           {isError && (
-            <span className="adviceContent__error-message">
+            <span>
               ⚠️
               <br />
               Request took too long or something wrong with internet. Please try
@@ -34,14 +41,21 @@ function App() {
           )}
         </div>
       )}
-      <div className="decorationLine">
+      <div className="mx-auto mb-6.25">
         <picture>
           <source srcSet={dividerMobile} media="(max-width: 37.5em)" />
           <img src={dividerDesktop} alt="divider image" />
         </picture>
       </div>
-      <button className="btn" onClick={fetchAdvice}>
-        <img className="btn__icon" src={buttonImg} alt="dice icon" />
+      <button
+        className="absolute bottom-0 left-1/2 transform-[translate(-50%,50%)] w-20 h-20 rounded-full bg-green cursor-pointer transition-all duration-200 hover:shadow-green-glow max-sm:w-17.5 max-sm:h-17.5"
+        onClick={fetchAdvice}
+      >
+        <img
+          className="w-7.5 h-7.5 mx-auto max-sm:w-6.5 max-sm:h-6.5"
+          src={buttonImg}
+          alt="dice icon"
+        />
       </button>
     </main>
   );
